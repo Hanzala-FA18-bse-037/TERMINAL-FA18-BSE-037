10787e91617dce089db1d127e539a866c73d61a7
Hanzala-FA18-bse-037 <FA18-BSE-037@cuilahore.edu.pk>
2021-06-22 18:42:52 +0500

    rat des

--- models/product.js
+++ models/product.js
@@ -3,6 +3,8 @@ var mongoose = require("mongoose");
 var productSchema = mongoose.Schema({
   name: String,
   price: String,
+  description: String,
+  Rating: String,
 });
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
