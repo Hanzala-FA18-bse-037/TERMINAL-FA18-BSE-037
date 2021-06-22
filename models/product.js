b5fb7791992016fa96989020280e8506231ab538
Hanzala-FA18-bse-037 <FA18-BSE-037@cuilahore.edu.pk>
2021-06-22 17:55:03 +0500

    COMMIT

--- models/product.js
+++ models/product.js
@@ -3,6 +3,12 @@ var mongoose = require("mongoose");
 var productSchema = mongoose.Schema({
   name: String,
   price: String,
+  description: String,
+  rating{
+    type: String,
+    min = 0,
+    max = 5
+  },
 });
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
